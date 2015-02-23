ccab692f13afed20b41339b8a396a4545dad9e6e
mAAdhaTTah <jamesorodig@gmail.com>
2015-02-23 01:08:52 +0100

    Add unit test stubs (marked incomplete)

---
+++ tests/test-cli.php
@@ -0,0 +1,13 @@
+<?php
+
+class WordPress_GitHub_Sync_CLI_Test extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+	}
+
+	function test_something() {
+		$this->markTestIncomplete();
+	}
+}
+
---
+++ tests/test-main.php
@@ -0,0 +1,13 @@
+<?php
+
+class WordPress_GitHub_Sync_Main_Test extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+	}
+
+	function test_something() {
+		$this->markTestIncomplete();
+	}
+}
+
