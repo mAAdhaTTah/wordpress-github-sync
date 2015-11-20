23680e7ee08494a2fb66bd82db22fd8e09269bea
mAAdhaTTah <jamesorodig@gmail.com>
2015-11-20 08:15:33 -0500

    Remove help text escaping
    Current escaping makes the HTML help text appear, since it's escaped.
    We actually want the HTML to be parsed, so we need to remove the
    escaping.
    
    Fixes #118.

--- views/setting-field.php
+++ views/setting-field.php
@@ -7,4 +7,4 @@
 ?>
 <?php $value = get_option( $args['name'], $args['default'] ); ?>
 <input name="<?php echo esc_attr( $args['name'] ); ?>" id="<?php echo esc_attr( $args['name'] ); ?>" type="text" value="<?php echo esc_attr( $value ); ?>" />
-<p class="description"><?php echo esc_html( $args['help_text'] ); ?></p>
+<p class="description"><?php echo $args['help_text']; ?></p>
