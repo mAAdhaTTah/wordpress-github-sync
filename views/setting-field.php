8e9e7f90ffd3e2f09465d00ee24f0d1a68d1ed09
mAAdhaTTah <jamesorodig@gmail.com>
2016-01-17 18:58:04 -0500

    Ensure secret is a password field
    This is kind of a hacky way of doing it, but should be ok unless
    we need another password field.
    
    Fix #124

--- views/setting-field.php
+++ views/setting-field.php
@@ -6,5 +6,5 @@
 
 ?>
 <?php $value = get_option( $args['name'], $args['default'] ); ?>
-<input name="<?php echo esc_attr( $args['name'] ); ?>" id="<?php echo esc_attr( $args['name'] ); ?>" type="text" value="<?php echo esc_attr( $value ); ?>" />
+<input name="<?php echo esc_attr( $args['name'] ); ?>" id="<?php echo esc_attr( $args['name'] ); ?>" type="<?php echo 'wpghs_secret' === $args['name'] ? 'password' : 'text'; ?>" value="<?php echo esc_attr( $value ); ?>" />
 <p class="description"><?php echo $args['help_text']; ?></p>
