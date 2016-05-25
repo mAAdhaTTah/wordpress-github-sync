bd749fc50cb6206bf5151ec35cfad06315924b73
Jonathan Carroll <jono@jcarroll.com.au>
2016-05-25 13:16:14 +0000

    added new composite function with shortcode

--- helpers.php
+++ helpers.php
@@ -48,3 +48,52 @@ function get_the_github_edit_url() {
 	return $wpghs_post->github_edit_url();
 }
 add_shortcode( 'get_the_github_edit_url', 'get_the_github_edit_url' );
+
+
+/**
+ * EXAMPLE FROM https://codex.wordpress.org/Shortcode_API
+ * // [bartag foo="foo-value"]
+ * function bartag_func( $atts ) {
+ *     $a = shortcode_atts( array(
+ *         'foo' => 'something',
+ *         'bar' => 'something else',
+ *     ), $atts );
+ * 
+ *     return "foo = {$a['foo']}";
+ * }
+ * add_shortcode( 'bartag', 'bartag_func' );
+*/
+
+/**
+ * Function with attributes
+ *   - type: 'link' to return a HTML anchor tag with text, or 'url' for bare URL.
+ *   - text: text to be included in the link. Ignored if type='url'.
+ * 
+ * Returns either a HTML formatted anchor tag or the bare URL of the current post on GitHub.
+ *
+ * @return string
+ */
+function write_wpghs_link( $atts ) {
+
+   extract(shortcode_atts(array(
+       "type" => 'link',
+       "text" => 'View this post on GitHub.'
+   ), $atts));
+
+   $output = '';
+
+   switch($type) {
+      case 'link':
+         $output .= '<a href="' . get_the_github_view_url() . '">' . apply_filters( 'wpghs_view_link_text', $text ) . '</a>';
+         break;
+      case 'url':
+         $output .= get_the_github_view_url();
+         break;
+      default:
+         $output .= get_the_github_view_url();
+   }
+
+   return($output);
+
+}
+add_shortcode( 'wpghs', 'write_wpghs_link' );
