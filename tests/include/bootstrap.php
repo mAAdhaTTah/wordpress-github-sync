582faf083674d6f50c8a8d0ac79707c327424580
mAAdhaTTah <jamesorodig@gmail.com>
2016-12-06 21:55:44 -0500

    Correctly suppress deprecated errors in tests
    This ensures the PHP7+ tests pass. WP sets its error reporting and was
    overwriting ours.

--- tests/include/bootstrap.php
+++ tests/include/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting( E_ALL  & ~E_DEPRECATED );
-
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
@@ -18,3 +16,5 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 require $_tests_dir . '/includes/bootstrap.php';
 
 require dirname( __FILE__ ) . '/../../vendor/jdgrimes/wp-http-testcase/wp-http-testcase.php';
+
+error_reporting( E_ALL ^ E_DEPRECATED );
