b96f6788ad8da3fc44605cf30aa6c1bc518efff8
mAAdhaTTah <jamesorodig@gmail.com>
2017-07-01 15:48:52 -0400

    Remove handling of custom post meta
    It's not possible to handle this consistently across all possible
    meta structures, so remove any handling from WPGHS and
    force userland to handle this explicitly, based on the way they
    need to edit, import, and export the post meta.

--- lib/post.php
+++ lib/post.php
@@ -331,17 +331,6 @@ class WordPress_GitHub_Sync_Post {
 			'published'    => 'publish' === $this->status() ? true : false,
 		);
 
-		//convert traditional post_meta values, hide hidden values, skip already populated values
-		foreach ( get_post_custom( $this->id ) as $key => $value ) {
-
-			if ( '_' === substr( $key, 0, 1 ) || isset( $meta[ $key ] ) ) {
-				continue;
-			}
-
-			$meta[ $key ] = $value;
-
-		}
-
 		return apply_filters( 'wpghs_post_meta', $meta, $this );
 	}
 
